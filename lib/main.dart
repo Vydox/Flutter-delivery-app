62a037cabb33c534ce3acd9a948f4cf01420e786
Vydox <makkor021@mail.ru>
2023-07-24 21:21:24 +0300

    [PT-2] Theme and navigation Minor fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,21 @@
+import 'package:core/config/di/app_di.dart';
+import 'package:core/config/di/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MealApp());
+import 'food_app/food_app.dart';
 
-class MealApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Meal App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MealScreen(),
-    );
-  }
-}
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-class MealScreen extends StatelessWidget {
-  final List<String> meals = List.generate(
-    100,
-      (i) => 'Meal $i',
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Meals'),
-      ),
-      body: ListView.builder(
-        itemCount: meals.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Column(
-            children: [
-              ListTile(
-                title: Row(
-                  children: [
-                    Expanded(
-                      child: Text(meals[index]),
-                    ),
-                    const Expanded(
-                        child: Text('Price: ')
-                    ),
-                    ElevatedButton.icon(
-                      icon: const Icon(Icons.add_shopping_cart),
-                      label: const Text('Add'),
-                      onPressed: () {
-                        //Add-to-cart logic here
-                      },
-                    ),
-                  ],
-                ),
-              ),
-              const Divider(
-                thickness: 3,
-              ),
-            ],
-          );
-        },
-      ),
-    );
-  }
+  appDI.initDependencies();
+
+  runApp(
+    const FoodApp(),
+  );
 }
+
